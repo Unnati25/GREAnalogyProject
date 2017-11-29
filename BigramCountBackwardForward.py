71655858fbf2e8583939f32cb0d48b57d097097d
Unnati <singhunnati25@gmail.com>
2017-11-29 15:21:05 -0600

    Forward and Backward Bigram

--- BigramCountBackwardForward.py
+++ BigramCountBackwardForward.py
@@ -11,7 +11,9 @@ def readFiles():
         with open(files, "r") as file:
             for line in file:
                 previousWord = ""
-                words = line.split()
+                line = re.sub("[^A-Za-z\[\] ] ", "", line)
+                line = re.sub("\s+", " ", line)
+                words = line.split(" ")
                 for word in words:
                     if word in unigramCount:
                         unigramCount[word] += 1
@@ -19,8 +21,6 @@ def readFiles():
                         unigramCount[word] = 1
                     if previousWord is not None:
                         bigram = previousWord + " " + word
-                        #print("Bigram constructed*****************")
-                        #print(bigram)
                         if bigram in bigramTotalWordCount:
                             bigramTotalWordCount[bigram] += 1
                         else:
@@ -31,20 +31,19 @@ def readFiles():
 
     for bigramCount in bigramTotalWordCount.values():
         totalCount +=bigramCount
-        uniqueWordCount = len(unigramCount)
-        testAnswerArray = []
-        answerIntToString ={}
-        answerIntToString[0] = ''
-        answerIntToString[1] = 'a'
-        answerIntToString[2] = 'b'
-        answerIntToString[3] = 'c'
-        answerIntToString[4] = 'd'
-        answerIntToString[5] = 'e'
+    uniqueWordCount = len(unigramCount)
+    testAnswerArray = []
+    answerIntToString ={}
+    answerIntToString[0] = ''
+    answerIntToString[1] = 'a'
+    answerIntToString[2] = 'b'
+    answerIntToString[3] = 'c'
+    answerIntToString[4] = 'd'
+    answerIntToString[5] = 'e'
     with open("C:/Users/Unnati/Documents/NLP/Project/data/Holmes.machine_format.questions.txt", "r") as file:
         answerNumber =1
         maxAnswerNumber =0
         maxSentenceProbability =0.0
-        global line
         for line in file:
             if answerNumber == 6:
                 testAnswerArray.append(answerIntToString[maxAnswerNumber])
@@ -52,8 +51,8 @@ def readFiles():
                 maxSentenceProbability = 0.0
                 maxAnswerNumber = 0
             line = line[line.index(')')+1:len(line)]
-            line = line.replace("[^A-Za-z\[\] ] ","")
-            line = line.replace("\s+"," ")
+            line = re.sub("[^A-Za-z\[\] ] ","", line)
+            line = re.sub("\s+"," ",line)
             line = line.strip()
             words = line.split(" ")
             targetWordIndex = 0
@@ -77,7 +76,7 @@ def readFiles():
             if targetWordIndex > 0 and words[targetWordIndex -1] is not None:
                 if (words[targetWordIndex-1] in unigramCount) and (backwardBigram is not None):
                     if backwardBigram in bigramTotalWordCount:
-                        backwardbigramProbability = (float)(bigramTotalWordCount[backwardBigram]+ 1)/(unigramCount[words[targetWordIndex-1]]+ uniqueWordCount)
+                        backwardbigramProbability = (float)(bigramTotalWordCount[backwardBigram]+ 1)/(unigramCount[words[targetWordIndex-1]])+ uniqueWordCount
                     else:
                         backwardbigramProbability = (float)(1/(unigramCount[words[targetWordIndex - 1]])) + uniqueWordCount
                 else:
@@ -88,7 +87,7 @@ def readFiles():
                         backwardbigramProbability = (float)(1/uniqueWordCount)
             if targetWordIndex < len(words) - 1 and words[targetWordIndex + 1] in unigramCount and forwardBigram is not None:
                 if forwardBigram in bigramTotalWordCount:
-                    forwardBigramProbability = (float)(bigramTotalWordCount[forwardBigram] + 1)/(unigramCount[words[targetWordIndex + 1]] + uniqueWordCount)
+                    forwardBigramProbability = (float)(bigramTotalWordCount[forwardBigram] + 1)/(unigramCount[words[targetWordIndex + 1]])+ uniqueWordCount
                 else:
                     forwardBigramProbability = (float)(1/(unigramCount[words[targetWordIndex + 1]])) + uniqueWordCount
             else:
@@ -114,7 +113,7 @@ def readFiles():
     for i in range(0, len(testActualAnswerArray)):
         if testAnswerArray[i] is not testActualAnswerArray[i]:
             testErrorCount = testErrorCount + 1
-
+    print(testErrorCount)
     print("Test Answer prediction Accuracy ")
     print((float)(len(testActualAnswerArray) - testErrorCount) * 100 / len(testAnswerArray))
     print("%")
