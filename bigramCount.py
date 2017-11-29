9d532ce6af0b019d13dc75b805096841a3a7eb09
Unnati <singhunnati25@gmail.com>
2017-11-29 13:59:03 -0600

    Improved accuracy from 18-22%

--- bigramCount.py
+++ bigramCount.py
@@ -1,4 +1,5 @@
 import glob
+import re
 
 path = "/Users/Unnati/Documents/NLP/Project/data/training_data/Holmes_Training_Data/"
 global index
@@ -9,8 +10,10 @@ def readFiles():
     for files in glob.glob(path + '*.txt'):
         with open(files, "r") as file:
             for line in file:
-                previousWord = ""
-                words = line.split()
+                line = re.sub("[^A-Za-z]", " ", line)
+                line = re.sub("\s+", " ", line)
+                previousWord = None
+                words = line.split(" ")
                 for word in words:
                     if word in unigramCount:
                         unigramCount[word] += 1
@@ -18,33 +21,38 @@ def readFiles():
                         unigramCount[word] = 1
                     if previousWord is not None:
                         bigram = previousWord + " " + word
+                        #print(bigram)
                         if bigram in bigramTotalWordCount:
                             bigramTotalWordCount[bigram] += 1
                         else:
                             bigramTotalWordCount[bigram] = 1
                     previousWord = word
+                    #print(previousWord)
 
-    totalCount = 0;
+    totalCount = 0
     for element in bigramTotalWordCount.keys():
         if bigramTotalWordCount[element]>20:
             del bigramTotalWordCount[element]
 
     for bigramCount in bigramTotalWordCount.values():
         totalCount +=bigramCount
-        uniqueWordCount = len(bigramTotalWordCount)
-        testAnswerArray = []
-        answerIntToString ={}
-        answerIntToString[0] = ''
-        answerIntToString[1] = 'a'
-        answerIntToString[2] = 'b'
-        answerIntToString[3] = 'c'
-        answerIntToString[4] = 'd'
-        answerIntToString[5] = 'e'
+
+    #print(totalCount)
+    uniqueWordCount = len(bigramTotalWordCount)
+    print(uniqueWordCount)
+    testAnswerArray = []
+    answerIntToString ={}
+    answerIntToString[0] = ''
+    answerIntToString[1] = 'a'
+    answerIntToString[2] = 'b'
+    answerIntToString[3] = 'c'
+    answerIntToString[4] = 'd'
+    answerIntToString[5] = 'e'
     with open("C:/Users/Unnati/Documents/NLP/Project/data/Holmes.machine_format.questions.txt", "r") as file:
         answerNumber =1
         maxAnswerNumber =0
-        maxSentenceProbability =0.0
-        global line
+        maxSentenceProbability = 0.0
+        global line, word
         for line in file:
             if answerNumber == 6:
                 testAnswerArray.append(answerIntToString[maxAnswerNumber])
@@ -52,28 +60,30 @@ def readFiles():
                 maxSentenceProbability = 0.0
                 maxAnswerNumber = 0
             line = line[line.index(')')+1:len(line)]
-            line = line.replace("[^A-Za-z]"," ")
-            line = line.replace("\s+"," ")
+            line = re.sub("[^A-Za-z]"," ",line)
+            line = re.sub("\s+"," ",line)
             line = line.strip()
             words = line.split(" ")
             sentenceProbability = 1.0
             previousWord = None
             for word in words:
+                #print(word)
                 if previousWord is not None:
                     bigram = previousWord + " " + word
                     bigramProbability = 0.0
-                    if previousWord in uniqueWordCount:
+                    if previousWord in unigramCount:
                         if bigram in bigramTotalWordCount:
-                            bigramProbability = (float)(bigramTotalWordCount[bigram] + 1)/(uniqueWordCount[previousWord] + uniqueWordCount)
+                            bigramProbability = (float)(bigramTotalWordCount[bigram] + 1)/(unigramCount[previousWord]) + uniqueWordCount
+                            print (bigramProbability)
                         else:
-                            bigramProbability = (float)(1/(uniqueWordCount[previousWord] + uniqueWordCount))
-                else:
-                    if bigram in bigramTotalWordCount:
-                        bigramProbability = (float)(bigramTotalWordCount[bigram] + 1)/uniqueWordCount
+                            bigramProbability = (float)(1/(unigramCount[previousWord])) + uniqueWordCount
                     else:
-                        bigramProbability = (float)(1/uniqueWordCount)
-                sentenceProbability = sentenceProbability * bigramProbability
-            previousWord = word
+                        if bigram in bigramTotalWordCount:
+                            bigramProbability = (float)(bigramTotalWordCount[bigram] + 1)/uniqueWordCount
+                        else:
+                            bigramProbability = (float)(1/uniqueWordCount)
+                    sentenceProbability = sentenceProbability * bigramProbability
+                previousWord = word
             if maxSentenceProbability < sentenceProbability:
                 maxSentenceProbability = sentenceProbability
                 maxAnswerNumber = answerNumber
@@ -92,7 +102,7 @@ def readFiles():
         if testAnswerArray[i] is not testActualAnswerArray[i]:
             testErrorCount = testErrorCount + 1
     print("Test Answer prediction Accuracy ")
-    print((float)(len(testActualAnswerArray) - testErrorCount) * 100/ len(testAnswerArray))
+    print(float)(len(testActualAnswerArray) - testErrorCount) * 100/ len(testAnswerArray)
     print("%")
 
 def main():
